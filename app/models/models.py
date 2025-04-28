eb2e5d8611823c8bc3f1356d3c9de09ccaf0d20f
John Christian Vicente <johnc.vicente1@gmail.com>
2025-04-29 02:33:11 +0800

    added reports

--- app/models/models.py
+++ app/models/models.py
@@ -1,5 +1,4 @@
-from app.models.conductors import Conductor
-from app.models.drivers import BusDriver, CarDriver
+from app.models.reports import Reports
 from app.models.vehicles import Bus, Car
 
 from .users import User
@@ -12,9 +11,8 @@ def create_tables():
     Car.create_table()
     Bus.create_index()
     Car.create_index()
-    BusDriver.create_table()
-    CarDriver.create_table()
-    Conductor.create_table()
-    BusDriver.create_index()
-    CarDriver.create_index()
-    Conductor.create_index()
+
+    Reports.create_bus_table()
+    Reports.create_car_table()
+    Reports.create_bus_trip_index()
+    Reports.create_car_trip_index()
